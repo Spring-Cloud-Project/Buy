768a22e7c608d3fe9017eda87160b0cf8d798b39
Cchua <vipcchua@hotmail.com>
2017-03-07 14:07:32 +0800

    SQL语句更新
    SQL语句更新

--- Spring-Cloud-Service-1/src/main/java/com/CchuaSpace/Mapper/CommunityRelativesMapper.java
+++ Spring-Cloud-Service-1/src/main/java/com/CchuaSpace/Mapper/CommunityRelativesMapper.java
@@ -21,6 +21,8 @@ import com.CchuaSpace.Model.CommodityCatalog;
 import com.CchuaSpace.Model.CommodityCatalogSql;
 import com.CchuaSpace.Model.CommodityInfo;
 import com.CchuaSpace.Model.CommodityInfoSql;
+import com.CchuaSpace.Model.CommunityRelatives;
+import com.CchuaSpace.Model.CommunityRelativesSql;
 import com.CchuaSpace.Model.TableUser;
 import com.CchuaSpace.Model.TableUserSql;
 
@@ -34,48 +36,59 @@ import com.CchuaSpace.Model.TableUserSql;
 public interface CommunityRelativesMapper {
 	/*--------------- -----<----*查询*---->--- ----------------------*/
 	
-	@Select("SELECT * FROM commodity_catalog where parents_id = #{parentsId}"
+	@Select("SELECT * FROM community_relatives where parents_id = #{parentsId}")
 			/*+ " AND commodity_catalog.parents_id = #{parentsId}"*/
-			+ " AND commodity_catalog.depth = #{depth}")
-	public List<CommodityCatalog> SelectCatalog(CommodityCatalog commodityCatalog);
+			
+	public List<CommunityRelatives> SelectByparentsId(CommunityRelatives communityRelatives);
 	
 
+	@Select("SELECT * FROM community_relatives where commodity_number = #{commodityNumber}")
+	/*+ " AND commodity_catalog.parents_id = #{parentsId}"*/
+	public List<CommunityRelatives> SelectByCommodityNumber(CommunityRelatives communityRelatives);
+
+
+
+	@Select("SELECT * FROM community_relatives where"
+			+ " commodity_number = #{commodityNumber} And parents_id = #{parentsId}")
+	/*+ " AND commodity_catalog.parents_id = #{parentsId}"*/
+	public List<CommunityRelatives> SelectByprcn(CommunityRelatives communityRelatives);
+
+
+	
 	
 	
 	/*--------------- -----<----*增加*---->--- ----------------------*/
 
 
 	
-	@InsertProvider(type = CommodityCatalogSql.class, method = "InsertCommodityCatalog")  
-	public List<CommodityCatalog> InsertCommodityInfo(CommodityCatalog commodityCatalog);
+	@InsertProvider(type = CommunityRelativesSql.class, method = "InsertCommunityRelatives")  
+	public List<CommunityRelatives> InsertCommunityRelatives(CommunityRelatives communityRelatives);
 	
 	
 	
 	
 	/*--------------- -----<----*删除*---->--- ----------------------*/
 
-	@Delete("Delete FROM commodity_catalog where parents_id = #{parentsId}"
-			+ " AND commodity_catalog.depth = #{depth}"
-			+ " And commodity_catalog.node_name = #{nodeName}")
-	int DeleteCatalog(CommodityCatalog commodityCatalog);
+	@Delete("Delete FROM community_relatives where parents_id = #{parentsId}"
+			+ " AND community_relatives.commodity_number = #{commodityNumber}")
+	int DeleteCatalog(CommunityRelatives communityRelatives);
 	
 
 	
 	
-	@Delete("Delete FROM commodity_catalog where catalog_id = #{catalogId}"
-			)
-	int DeleteCatalogById(CommodityCatalog commodityCatalog);
+	@Delete("Delete FROM community_relatives where id = #{Id}")
+	int DeleteCatalogById(CommunityRelatives communityRelatives);
 	
 	
-	
+		
 	/*--------------- -----<----*修改*---->--- ----------------------*/
 
 
 
-	@Update("UPDATE commodity_catalog SET commodity_catalog.node_name = #{nodeName} "
-			+ "Where commodity_catalog.catalogId = #{catalogId}"
+	@Update("UPDATE community_relatives SET community_relatives.parents_id = #{parentsId} "
+			+ "Where community_relatives.commodity_number = #{commodityNumber}"
 			)
-	int UpdateCatalog(CommodityCatalog commodityCatalog);
+	int UpdateCatalog(CommunityRelatives communityRelatives);
 	
 	
 
